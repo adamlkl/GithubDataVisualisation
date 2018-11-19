bdf33c8879e300f4bd7566bf24d2027b71dd5061
adamlkl <kleong@tcd.ie>
2018-11-19 10:07:33 +0000

    formulate contributors and languages

--- Access.py
+++ Access.py
@@ -1,4 +1,5 @@
 from github import Github
+import json
 
 token = "fceed303e67495273a75c12b170080e7b515fcc9"
 
@@ -8,29 +9,42 @@ def crawler():
     g = Github(token)
     user = g.get_user("Bloomberg")
 
-    for repo in user.get_repos():
-        print(repo.name)
-
-    print("Repository Name: bqplot")
-    repo2 = user.get_repos("Bloomberg/bde")
-    for repo in repo2:
-        topics = repo.get_topics()
-        for c in topics:
+    for repo7 in user.get_repos():
+        print(repo7.name)
+        print(repo7.size)
+        y = repo7.get_languages()
+        for x in y:
+            print(x)
+            print(y[x])
+        con = repo7.get_contributors()
+        for c in con:
             print(c)
 
-    print("Contents: ")
-    contents = repo.get_contents("")
-    for content_file in contents:
-        print(content_file)
 
-    print("List of Branches:")
-    list(repo.get_branches())
-
-    print("Contributors:")
-    contributors = repo.get_contributors()
-    for contributor in contributors:
-        print(contributor.login)
-
+    data = {}
+    data["Contributors"] = {}
+'''
+    for repo in user.get_repos():
+        print(repo.name)
+        contributors = repo.get_stats_contributors()
+        data["Contributors"][""+repo.name] = []
+        for c in contributors:
+            total_insertions = 0
+            total_deletions = 0
+            total_contribution = 0
+            for week in c.weeks:
+                total_insertions += week.a
+                total_deletions += week.d
+                total_contribution += (week.a + week.d)
+            data["Contributors"][""+repo.name].append({
+                "Name": c.author.login,
+                "Insertions": total_insertions,
+                "Deletions": total_deletions,
+                "Contributions": total_contribution
+            })
+    with open('contributors_data.json', 'w') as outfile:
+        json.dump(data, outfile)
+'''
 
 def main():
     crawler()
--- access_test.py
+++ access_test.py
@@ -10,6 +10,20 @@ class GithubAccessTest(unittest.TestCase):
         self.assertEqual(user.login, u'bloomberg')
         self.assertEqual(user.name, 'Bloomberg')
 
+        print("Repository Name: bqplot")
+        repo2 = user.get_repos("Bloomberg/bde")
+        for repo in repo2:
+            topics = repo.get_topics()
+            for c in topics:
+                print(c)
+
+        print("Contents: ")
+        contents = repo.get_contents("")
+        for content_file in contents:
+            print(content_file)
+
+        print("List of Branches:")
+        list(repo.get_branches())
 
 if __name__ == '__main__':
     unittest.main()
